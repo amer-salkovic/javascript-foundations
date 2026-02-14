922546cfdf593a276b1713990ba6a0f3c0d93ab9
amer-salkovic <amersalkovic53@gmail.com>
2026-02-14 12:23:22 +0100

    day 3 completed

---
+++ day-3/index.js
@@ -0,0 +1,18 @@
+// function isThruty(value){
+//     if(value){
+//         return true;
+//     }
+//
+//     else {
+//         return false;
+//     }
+//
+//
+// }
+//
+// console.log(isTruthy("0") === true);
+// console.log(isTruthy(0) === false);
+// console.log(isTruthy([]) === true);
+// console.log(isTruthy("") === false);
+// console.log(isTruthy(null) === false);
+// console.log(isTruthy(" ") === true);
